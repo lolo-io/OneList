3accf9ca6bff51f5f78833e376816519d4c04691
Felix Nüsse <felix.nuesse@t-online.de>
2020-04-02 15:39:59 +0200

    Show undo dialog when deleting and store deleted items

--- app/src/main/java/com/lolo/io/onelist/model/Item.kt
+++ app/src/main/java/com/lolo/io/onelist/model/Item.kt
@@ -2,7 +2,9 @@ package com.lolo.io.onelist.model
 
 import com.google.gson.annotations.SerializedName
 
-data class Item(@SerializedName("title") var title: String = "", @SerializedName("comment") var comment: String = "", @SerializedName("done") var done: Boolean = false, @SerializedName("commentDisplayed") var commentDisplayed: Boolean = false, @SerializedName("stableId") val stableId: Long = System.currentTimeMillis()) {
+const val ITEM_NOT_DELETED: Long = -1
+
+data class Item(@SerializedName("title") var title: String = "", @SerializedName("comment") var comment: String = "", @SerializedName("deleted") var deleted: Long = ITEM_NOT_DELETED, @SerializedName("done") var done: Boolean = false,@SerializedName("commentDisplayed") var commentDisplayed: Boolean = false, @SerializedName("stableId") val stableId: Long = System.currentTimeMillis()) {
     override fun toString(): String {
         var string = "${if (done) "x" else "o"} $title"
         if (comment.isNotBlank()) string += "\n    ${comment.replace("\n", "\n    ")}"
