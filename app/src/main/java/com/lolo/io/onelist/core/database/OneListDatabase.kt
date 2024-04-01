9153b1abb22afa3d13f120dcb7f74a9d076eefbf
Loïc Teyssier <1267636+lolo-io@users.noreply.github.com>
2024-04-01 14:25:17 +0200

    Fix non unique items ids crashes - Use SecureRandom when batch updati… (#67)

--- app/src/main/java/com/lolo/io/onelist/core/database/OneListDatabase.kt
+++ app/src/main/java/com/lolo/io/onelist/core/database/OneListDatabase.kt
@@ -1,8 +1,11 @@
 package com.lolo.io.onelist.core.database
 
+import androidx.room.AutoMigration
 import androidx.room.Database
+import androidx.room.DeleteColumn
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.AutoMigrationSpec
 import com.lolo.io.onelist.core.database.dao.ItemListDao
 import com.lolo.io.onelist.core.database.model.ItemEntity
 import com.lolo.io.onelist.core.database.model.ItemListEntity
@@ -10,9 +13,15 @@ import com.lolo.io.onelist.core.database.util.Converters
 
 @Database(
     entities = [ItemListEntity::class, ItemEntity::class],
-    version = 1
+    version = 2,
+    autoMigrations = [
+        AutoMigration(from = 1, to = 2, spec = OneListDatabase.Migration1To2::class)
+    ]
 )
 @TypeConverters(Converters::class)
 abstract class OneListDatabase: RoomDatabase() {
     abstract val itemListDao: ItemListDao
+
+    @DeleteColumn("item", "stableId")
+    class Migration1To2 : AutoMigrationSpec
 }
